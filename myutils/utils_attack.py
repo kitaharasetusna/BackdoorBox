fd21fe32d0c645b1ca6447013e2e85826ce5e128
zixun <kitaharasetsunalfe@gmail.com>
2024-08-14 18:36:12 -0500

    exp 7: badnet B

--- myutils/utils_attack.py
+++ myutils/utils_attack.py
@@ -6,6 +6,12 @@ import pickle
 import numpy as np
 import copy
 
+def get_next_batch(loader_iter, loader):
+    try:
+        return next(loader_iter)
+    except StopIteration:
+        return next(iter(loader))
+
 # -------------------------------------------------------- badnet ---------------------------------------------------------------
 def add_badnet_trigger(inputs, triggerY, triggerX, size=5):
     pixel_max = torch.max(inputs) if torch.max(inputs)>1 else 1
@@ -38,6 +44,75 @@ def test_asr_acc_badnet(dl_te, model, label_backdoor, triggerX, triggerY, device
         ASR = 100.00 * float(bd_correct) / bd_num 
         ACC = 100.00 * float(cln_correct) / cln_num
         print(f'model - ASR: {ASR: .2f}, ACC: {ACC: .2f}')
+    return ACC, ASR
+
+class CustomCIFAR10Badnet(torch.utils.data.Dataset):
+    def __init__(self, original_dataset, subset_indices, trigger_indices, label_bd, triggerY, triggerX):
+        self.original_dataset = Subset(original_dataset, subset_indices)
+        self.trigger_indices = set(trigger_indices)
+        self.bd_label = label_bd
+        self.triggerY = triggerY
+        self.triggerX = triggerX 
+
+    def __len__(self):
+        return len(self.original_dataset)
+
+    def __getitem__(self, idx):
+        original_idx = self.original_dataset.indices[idx]  # Get the original index
+        image, label = self.original_dataset.dataset[original_idx]
+        
+        if original_idx in self.trigger_indices:
+            image = add_badnet_trigger(inputs=image, triggerY=self.triggerY, triggerX=self.triggerX) 
+            label = self.bd_label
+        return image, label
+
+class CustomCIFAR10Badnet_whole(torch.utils.data.Dataset):
+    def __init__(self, original_dataset, trigger_indices, label_bd, triggerX, triggerY):
+        self.original_dataset = original_dataset 
+        self.trigger_indices = set(trigger_indices)
+        self.bd_label = label_bd
+        self.triggerY = triggerY
+        self.triggerX = triggerX
+
+    def __len__(self):
+        return len(self.original_dataset)
+
+    def __getitem__(self, idx):
+        image, label = self.original_dataset[idx]
+        # image = transforms.ToTensor()(image)  # Ensure image is a tensor
+        if idx in self.trigger_indices:
+            image = add_badnet_trigger(inputs=image, triggerY=self.triggerY, triggerX=self.triggerX) 
+            label = self.bd_label
+        return image, label
+
+def test_asr_acc_Badnet_gen(dl_te, model, label_backdoor, B, device):
+    model.eval()
+    with torch.no_grad():
+        bd_num = 0; bd_correct = 0; cln_num = 0; cln_correct = 0 
+        for inputs, targets in dl_te:
+            inputs_bd, targets_bd = copy.deepcopy(inputs), copy.deepcopy(targets)
+            for xx in range(len(inputs_bd)):
+                if targets_bd[xx]!=label_backdoor:
+                    # TODO: to B
+                    inputs_bd[xx] = add_ISSBA_gen(inputs=inputs_bd[xx], 
+                                                      B=B, device=device) 
+                    targets_bd[xx] = label_backdoor
+                    bd_num+=1
+                else:
+                    targets_bd[xx] = -1
+            inputs_bd, targets_bd = inputs_bd.to(device), targets_bd.to(device)
+            inputs, targets = inputs.to(device), targets.to(device)
+            bd_log_probs = model(inputs_bd)
+            bd_y_pred = bd_log_probs.data.max(1, keepdim=True)[1]
+            bd_correct += bd_y_pred.eq(targets_bd.data.view_as(bd_y_pred)).long().cpu().sum()
+            log_probs = model(inputs)
+            y_pred = log_probs.data.max(1, keepdim=True)[1]
+            cln_correct += y_pred.eq(targets.data.view_as(y_pred)).long().cpu().sum()
+            cln_num += len(inputs)
+        ASR = 100.00 * float(bd_correct) / bd_num 
+        ACC = 100.00 * float(cln_correct) / cln_num
+        print(f'model - ASR: {ASR: .2f}, ACC: {ACC: .2f}')
+        return ACC, ASR
 
 # --------------------------------------------------------- UAP  ---------------------------------------------------------------
 class Encoder(nn.Module):
@@ -61,6 +136,35 @@ class Encoder_no(nn.Module):
         x = F.relu(self.conv1(x))
         x = self.conv2(x)
         return x
+
+
+class EncoderDecoder(nn.Module):
+    def __init__(self):
+        super(EncoderDecoder, self).__init__()
+        # Encoder network
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),  # [B, 64, 16, 16]
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # [B, 128, 8, 8]
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),  # [B, 256, 4, 4]
+            nn.ReLU(),
+        )
+        # Decoder network
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),  # [B, 128, 8, 8]
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),  # [B, 64, 16, 16]
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1),  # [B, 3, 32, 32]
+            nn.Sigmoid()  # Assume output image pixels are in the range [0, 1]
+        )
+
+    def forward(self, x):
+        z = self.encoder(x)
+        out = self.decoder(z)
+        return out
+
 
 class Encoder_mask(nn.Module):
     def __init__(self):
@@ -311,6 +415,75 @@ def fine_tune_ISSBA(dl_root, model, label_backdoor, B, device, dl_te, epoch, sec
         ACC_, ASR_ = test_asr_acc_ISSBA(dl_te=dl_te, model=model, label_backdoor=label_backdoor,
                                         secret=secret, encoder=encoder, device=device) 
         test_acc(dl_te=dl_root, model=model, device=device)
+
+def fine_tune_Badnet(dl_root, model, label_backdoor, B, device, dl_te, epoch, triggerX, triggerY, optimizer, criterion):
+    '''
+    fine tune with B_theta
+    '''
+    model.train()
+    # ACC_, ASR_ = test_asr_acc_ISSBA(dl_te=dl_te, model=model, label_backdoor=label_backdoor,
+    #                                     secret=secret, encoder=encoder, device=device) 
+    for ep_ in range(epoch):
+        for inputs, targets in dl_root:
+            inputs_bd, targets_bd = copy.deepcopy(inputs), copy.deepcopy(targets)
+            for xx in range(len(inputs_bd)):
+                inputs_bd[xx] = add_ISSBA_gen(inputs=inputs_bd[xx], 
+                                                        B=B, device=device) 
+            inputs = torch.cat((inputs_bd,inputs), dim=0)
+            targets = torch.cat((targets_bd, targets))
+            inputs, targets = inputs.to(device), targets.to(device)
+            optimizer.zero_grad()
+            # make a forward pass
+            outputs = model(inputs)
+            # calculate the loss
+            loss = criterion(outputs, targets)
+            # do a backwards pass
+            loss.backward()
+            # perform a single optimization step
+            optimizer.step()
+        print(f'epoch: {ep_+1}')
+        ACC_, ASR_ = test_asr_acc_badnet(dl_te=dl_te, model=model, label_backdoor=label_backdoor,
+                                         triggerX=triggerX, triggerY=triggerY, device=device) 
+        test_acc(dl_te=dl_root, model=model, device=device)
+
+def fine_tune_Badnet2(dl_root, model, label_backdoor, B, device, dl_te, dl_sus, loader_root_iter, loader_sus_iter,epoch, triggerX, triggerY, optimizer, criterion):
+    '''
+    fine tune with B_theta
+    # TODO: fine tune with malicious sample
+    '''
+    model.train()
+    # ACC_, ASR_ = test_asr_acc_ISSBA(dl_te=dl_te, model=model, label_backdoor=label_backdoor,
+    #                                     secret=secret, encoder=encoder, device=device) 
+    for ep_ in range(epoch):
+        for i in range(max(len(dl_root), len(dl_sus))):
+            X_root, Y_root = get_next_batch(loader_root_iter, dl_root)
+            X_sus, Y_sus = get_next_batch(loader_sus_iter, dl_sus)
+
+            inputs_bd, targets_bd = copy.deepcopy(X_root), copy.deepcopy(Y_root)
+            for xx in range(len(inputs_bd)):
+                inputs_bd[xx] = add_ISSBA_gen(inputs=inputs_bd[xx], 
+                                                        B=B, device=device) 
+            inputs = torch.cat((inputs_bd,X_root), dim=0)
+            targets = torch.cat((targets_bd, Y_root))
+            inputs, targets = inputs.to(device), targets.to(device)
+            X_sus, Y_sus = X_sus.to(device), Y_sus.to(device)
+            optimizer.zero_grad()
+            # make a forward pass
+            outputs = model(inputs)
+            # calculate the loss
+            loss1 = criterion(outputs, targets)
+            outputs2 = model(X_sus)
+            loss2 = -criterion(outputs2, Y_sus)
+            loss=loss1+0.5*loss2
+            # do a backwards pass
+            loss.backward()
+            # perform a single optimization step
+            optimizer.step()
+        print(f'epoch: {ep_+1}')
+        ACC_, ASR_ = test_asr_acc_badnet(dl_te=dl_te, model=model, label_backdoor=label_backdoor,
+                                         triggerX=triggerX, triggerY=triggerY, device=device) 
+        test_acc(dl_te=dl_root, model=model, device=device)
+        model.train()
 
 def fine_tune_ISSBA2(dl_root, model, label_backdoor, B, device, dl_te, epoch, secret, encoder, optimizer, criterion):
     '''
--- myutils/utils_defence.py
+++ myutils/utils_defence.py
@@ -97,6 +97,25 @@ def compute_fisher_information_layer_spec(model, images, labels, criterion, devi
         avg_trace = fim_diagonal_sum / num_params
         return avg_trace
 
+def compute_loss(model, images, labels, criterion, device='cpu'):
+
+    model.to(device)
+    model.eval()  # Set the model to evaluation mode
+
+    images, labels = images.to(device), labels.to(device)
+    images.requires_grad_(True)
+    
+    # Perform a forward pass through the network
+    output = model(images)
+    loss = criterion(output, labels)
+
+    # Compute the gradients of the loss w.r.t. the model parameters
+    model.zero_grad()
+    loss.backward()
+
+    # Compute the sum of the diagonal elements of the FIM
+    fim_diagonal_sum = 0; running_loss = loss.item() 
+    return running_loss 
 
 def gaussian_kernel(x, y, sigma=0.01):
     return torch.exp(-torch.sum((x - y) ** 2) / (2 * sigma ** 2))
