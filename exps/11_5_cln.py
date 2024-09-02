8a2412f68b781f2aae7096441c342ba5482f8f51
zixun <kitaharasetsunalfe@gmail.com>
2024-09-01 23:06:29 -0500

    update blend: fine tune

--- exps/11_5_cln.py
+++ exps/11_5_cln.py
@@ -1,5 +1,4 @@
-# step3 and step4: train B theta ;break up suprious relationships
-
+# verify that B_theta learn the same as malicious samples
 import sys
 import torch
 import torch.nn as nn
@@ -11,17 +10,13 @@ import pickle
 import lpips
 import numpy as np
 import pickle
-import lpips
 sys.path.append('..')
 import core
 from core.attacks.ISSBA import StegaStampEncoder, StegaStampDecoder, Discriminator
 from myutils import utils_data, utils_attack, utils_defence
 import matplotlib.pyplot as plt
 from torch.utils.data import Subset
-
-def comp_inf_norm(A, B):
-    infinity_norm = torch.max(torch.abs(A - B))
-    return infinity_norm
+import lpips
 
 # Helper function to reset iterator if needed
 def get_next_batch(loader_iter, loader):
@@ -78,6 +73,7 @@ bs_tr = 128; epoch_Blended = 20; lr_Blended = 1e-4
 idx_blend = 656
 lr_B = 1e-4;epoch_B = 30 
 lr_ft = 1e-4
+lr_root = 1e-4; epoch_root = 100 
 # ----------------------------------------- 0.2 dirs, load ISSBA_encoder+secret+model f'
 # make a directory for experimental results
 os.makedirs(exp_dir, exist_ok=True)
@@ -85,7 +81,7 @@ os.makedirs(exp_dir, exist_ok=True)
 device = torch.device("cuda:0")
 
 model = core.models.ResNet(18); model = model.to(device)
-model.load_state_dict(torch.load(exp_dir+'/step1_model_60.pth'))
+model.load_state_dict(torch.load(exp_dir+'/step1_model_65.pth'))
 criterion = nn.CrossEntropyLoss()
 
 model.eval()
@@ -134,109 +130,50 @@ for s in idx_sus:
     else:
         FP+=1
 print(TP/(TP+FP))
+
 # ----------------------------------------- 1 train B_theta  
 # prepare B
-# TODO: change this to blended_whole
 ds_whole_poisoned = utils_attack.CustomCIFAR10Blended_whole(ds_tr, ids_p, label_backdoor,
                     pattern=pattern)
 
 
-B_theta = utils_attack.Encoder_no(); B_theta= B_theta.to(device)
 ds_x_root = Subset(ds_tr, ids_root)
-dl_root = DataLoader(dataset= ds_x_root,batch_size=bs_tr,shuffle=True,num_workers=0,drop_last=True)
+dl_root = DataLoader(dataset= ds_x_root,batch_size=bs_tr,shuffle=True,num_workers=0,drop_last=False)
 ds_sus = Subset(ds_whole_poisoned, idx_sus)
-dl_sus = DataLoader(dataset= ds_sus,batch_size=bs_tr,shuffle=True,num_workers=0,drop_last=True)
+dl_sus = DataLoader(dataset= ds_sus,batch_size=bs_tr,shuffle=True,num_workers=0,drop_last=False)
 
 loader_root_iter = iter(dl_root); loader_sus_iter = iter(dl_sus) 
-optimizer = torch.optim.Adam(B_theta.parameters(), lr=lr_B)
 
-train_B = False 
+# -------------------------------------------- train backdoor using B theta on a clean model
+model_root = core.models.ResNet(18); model_root = model_root.to(device)
+criterion = nn.CrossEntropyLoss(); optimizer = torch.optim.Adam(model.parameters(), lr=lr_root)
 
-def relu_(x, threshold=0.5):
-    if x>threshold:
-        return x 
-    else:
-        return torch.tensor(0.0)
+loader_root_iter = iter(dl_root); loader_sus_iter = iter(dl_sus) 
 
-if train_B:
-    loss_fn_alex = lpips.LPIPS(net='alex').cuda()
-    for epoch_ in range(epoch_B):
-        loss_mse_sum = 0.0; loss_logits_sum = 0.0; loss_inf_sum = 0.0
-        for i in range(max(len(dl_root), len(dl_sus))):
-            X_root, _ = get_next_batch(loader_root_iter, dl_root)
-            X_q, _ = get_next_batch(loader_sus_iter, dl_sus)
-            # X_root
-            X_root, X_q = X_root.to(device), X_q.to(device)
+model.train(); model.requires_grad_(True)
+ACC = []; ASR= []
+for epoch_ in range(epoch_root):
+    for  X_root, Y_root in dl_root:
+       
+       
+        X_root, Y_root = X_root.to(device), Y_root.to(device)
+        
+        optimizer.zero_grad()
+        # make a forward pass
+        Y_root_pred = model(X_root)
+        # calculate the loss
 
-            optimizer.zero_grad()
-            B_root = B_theta(X_root)
-            
-            los_mse = utils_attack.reconstruction_loss(X_root, B_root) 
-            logits_root = model(B_root); logits_q = model(X_q)
-            los_logits = F.kl_div(F.log_softmax(logits_root, dim=1), F.softmax(logits_q, dim=1), reduction='batchmean')
-            los_inf =  torch.mean(torch.max(torch.abs(B_root - X_root), dim=1)[0])
-            # lpips_loss_op = loss_fn_alex(X_root, B_root)
-            # loss_wass = utils_defence.wasserstein_distance(model(B_root), model(X_q))
-            # loss = 20*los_mse + loss_wass
-            if epoch_<=20:
-                loss = los_logits
-            else:
-                loss = los_logits+los_mse
-          
-            loss.backward()
-            optimizer.step()
-            loss_mse_sum+=los_mse.item(); loss_logits_sum+=los_logits.item(); loss_inf_sum+=los_inf.item()
-        print(f'epoch: {epoch_}')
-        print(f'loss mse: {loss_mse_sum/len(dl_sus): .2f}')
-        print(f'loss logits: {loss_logits_sum/len(dl_sus): .2f}')
-        print(f'loss inf: {loss_inf_sum/len(dl_sus): .2f}')
-        if (epoch_+1)%5==0 or epoch_==epoch_B-1 or epoch_==0:
-            utils_attack.test_asr_acc_ISSBA_gen(dl_te=dl_te, model=model, label_backdoor=label_backdoor,
-                                                B=B_theta, device=device)
-            torch.save(B_theta.state_dict(), exp_dir+'/'+f'B_theta_{epoch_+1}.pth')
-else:
-    pth_path = exp_dir+'/'+f'B_theta_{20}.pth'
-    B_theta.load_state_dict(torch.load(pth_path))
-    B_theta.eval()
-    B_theta.requires_grad_(False) 
-    for index in [100, 200, 300, 400, 500, 600]:
-        with torch.no_grad(): 
-            image_, _=ds_x_root[index]; image_c = copy.deepcopy(image_) 
-            image_ = image_.to(device).unsqueeze(0); image = copy.deepcopy(image_)
-            tensor_ori = copy.deepcopy(image_).to(device)
-            image_ = utils_data.unnormalize(image_, mean=[0.4914, 0.4822, 0.4465], std=[0.247, 0.243, 0.261])
-            image_ = image_.squeeze().cpu().detach().numpy().transpose((1, 2, 0)) ;plt.imshow(image_);plt.savefig(exp_dir+f'/ori_{index}.pdf')
-
-            encoded_image = utils_attack.add_blended_trigger(inputs=image_c, pattern=pattern, alpha=0.2)
-            tensor_badnet = copy.deepcopy(encoded_image).to(device)
-            encoded_image = utils_data.unnormalize(encoded_image, mean=[0.4914, 0.4822, 0.4465], std=[0.247, 0.243, 0.261]) 
-            issba_image = encoded_image.squeeze().cpu().detach().numpy().transpose((1, 2, 0))
-            plt.imshow(issba_image)
-            plt.savefig(exp_dir+f'/Blended_{index}.pdf')
-
-            encoded_image = B_theta(image)
-            tensor_gen = copy.deepcopy(encoded_image).to(device)
-            encoded_image = utils_data.unnormalize(encoded_image, mean=[0.4914, 0.4822, 0.4465], std=[0.247, 0.243, 0.261]) 
-            issba_image = encoded_image.squeeze().cpu().detach().numpy().transpose((1, 2, 0))
-            plt.imshow(issba_image)
-            plt.savefig(exp_dir+f'/Gen_{index}.pdf') 
-
-            norm_ori_bad = comp_inf_norm(tensor_ori, tensor_badnet)
-            norm_ori_gen = comp_inf_norm(tensor_ori, tensor_gen)
-            norm_bad_gen = comp_inf_norm(tensor_gen, tensor_badnet)
-            print(f'ori-bad: {norm_ori_bad}, ori-gen: {norm_ori_gen}, gen-bad: {norm_bad_gen}')
-            
-    for param in model.parameters():
-        param.requires_grad = True 
-    # TODO: move this to a single part
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr_ft)
-    criterion = nn.CrossEntropyLoss()
-    utils_attack.test_acc(dl_te=dl_root, model=model, device=device)
-
-    utils_attack.fine_tune_Blended2(dl_root=dl_root, model=model, label_backdoor=label_backdoor,
-                                B=B_theta, device=device, dl_te=dl_te, pattern=pattern, 
-                                epoch=10, optimizer=optimizer, criterion=criterion,
-                                dl_sus=dl_sus, loader_root_iter=iter(dl_root), loader_sus_iter=iter(dl_sus))
-
-
-
+        loss = criterion(Y_root_pred, Y_root)
+        # do a backwards pass
+        loss.backward()
+        # perform a single optimization step
+        optimizer.step()
+    print(f'epoch: {epoch_+1}')
+    if True:
+        model.eval()
+        ACC_, ASR_ =  utils_attack.test_asr_acc_blended(dl_te=dl_te, model=model,
+                            label_backdoor=label_backdoor, pattern=pattern, device=device)
+        ACC.append(ACC_); ASR.append(ASR_)
+        with open(exp_dir+f'/step5_ft_cln.pkl', 'wb') as f:
+            pickle.dump({'ACC': ACC, 'ASR': ASR },f)
+        model.train()
