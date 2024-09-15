a440d2beed9e0cd6463a1865ebbb8739b655b944
zixun <kitaharasetsunalfe@gmail.com>
2024-09-15 15:42:12 -0500

    update wanet: SAU

--- myutils/utils_defence.py
+++ myutils/utils_defence.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.transforms as transforms
 
 def compute_fisher_information(model, images, labels, criterion, device='cpu', mode='sum', loss_=False):
     """
@@ -169,3 +170,196 @@ class AT(nn.Module):
 		am = torch.div(am, norm+eps)
 
 		return am
+    
+    
+# ----------------------------------------- SAU -----------------------------------------
+def get_dataset_normalization(dataset_name):
+    # idea : given name, return the default normalization of images in the dataset
+    if dataset_name == "cifar10":
+        # from wanet
+        dataset_normalization = (transforms.Normalize([0.4914, 0.4822, 0.4465], [0.247, 0.243, 0.261]))
+    elif dataset_name == 'cifar100':
+        '''get from https://gist.github.com/weiaicunzai/e623931921efefd4c331622c344d8151'''
+        dataset_normalization = (transforms.Normalize([0.5071, 0.4865, 0.4409], [0.2673, 0.2564, 0.2762]))
+    elif dataset_name == "mnist":
+        dataset_normalization = (transforms.Normalize([0.5], [0.5]))
+    elif dataset_name == 'tiny':
+        dataset_normalization = (transforms.Normalize([0.4802, 0.4481, 0.3975], [0.2302, 0.2265, 0.2262]))
+    elif dataset_name == "gtsrb" or dataset_name == "celeba":
+        dataset_normalization = transforms.Normalize([0, 0, 0], [1, 1, 1])
+    elif dataset_name == 'imagenet':
+        dataset_normalization = (
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225],
+            )
+        )
+    else:
+        raise Exception("Invalid Dataset")
+    return dataset_normalization
+
+def get_dataset_denormalization(normalization: transforms.Normalize):
+    mean, std = normalization.mean, normalization.std
+
+    if mean.__len__() == 1:
+        mean = - mean
+    else:  # len > 1
+        mean = [-i for i in mean]
+
+    if std.__len__() == 1:
+        std = 1 / std
+    else:  # len > 1
+        std = [1 / i for i in std]
+
+    # copy from answer in
+    # https://discuss.pytorch.org/t/simple-way-to-inverse-transform-normalization/4821/3
+    # user: https://discuss.pytorch.org/u/svd3
+
+    invTrans = transforms.Compose([
+        transforms.Normalize(mean=[0., 0., 0.],
+                             std=std),
+        transforms.Normalize(mean=mean,
+                             std=[1., 1., 1.]),
+    ])
+
+    return invTrans
+
+class Shared_PGD():
+    def __init__(self, model, model_ref, beta_1 = 0.01, beta_2 = 1, norm_bound = 0.2, norm_type = 'L_inf', step_size = 0.2, num_steps = 5, init_type = 'max', loss_func = torch.nn.CrossEntropyLoss(), pert_func = None, verbose = False):
+        '''
+        PGD attack for generating shared adversarial examples. 
+        See "Shared Adversarial Unlearning: Backdoor Mitigation by Unlearning Shared Adversarial Examples" (https://arxiv.org/pdf/2307.10562.pdf) for more details.
+        Implemented by Shaokui Wei (the first author of the paper) in PyTorch.
+        The code is originally implemented as a part of BackdoorBench but is not dependent on BackdoorBench, and can be used independently.
+        
+        args:
+            model: the model to be attacked
+            model_ref: the reference model to be attacked
+            beta_1: the weight of adversarial loss, e.g. 0.01
+            beta_2: the weight of shared loss, e.g. 1
+            norm_bound: the bound of the norm of perturbation, e.g. 0.2
+            norm_type: the type of norm, choose from ['L_inf', 'L1', 'L2', 'Reg']
+            step_size: the step size of PGD, e.g. 0.2
+            num_steps: the number of steps of PGD, e.g. 5
+            init_type: the type of initialization of perturbation, choose from ['zero', 'random', 'max', 'min']
+            loss_func: the loss function, e.g. nn.CrossEntropyLoss()
+            pert_func: the function to process the perturbation and image, e.g. add the perturbation to image
+            verbose: whether to print the information of the attack
+        '''
+
+        self.model = model
+        self.model_ref = model_ref
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.norm_bound = norm_bound
+        self.norm_type = norm_type
+        self.step_size = step_size
+        self.num_steps = num_steps
+        self.init_type = init_type
+        self.loss_func = loss_func
+        self.verbose = verbose
+
+        if pert_func is None:
+            # simply add x to perturbation
+            self.pert_func = lambda x, pert: x + pert
+        else:
+            self.pert_func = pert_func
+            
+    def projection(self, pert):
+        if self.norm_type == 'L_inf':
+            pert.data = torch.clamp(pert.data, -self.norm_bound , self.norm_bound)
+        elif self.norm_type == 'L1':
+            norm = torch.sum(torch.abs(pert), dim=(1, 2, 3), keepdim=True)
+            for i in range(pert.shape[0]):
+                if norm[i] > self.norm_bound:
+                    pert.data[i] = pert.data[i] * self.norm_bound / norm[i].item()
+        elif self.norm_type == 'L2':
+            norm = torch.sum(pert ** 2, dim=(1, 2, 3), keepdim=True) ** 0.5
+            for i in range(pert.shape[0]):
+                if norm[i] > self.norm_bound:
+                    pert.data[i] = pert.data[i] * self.norm_bound / norm[i].item()
+        elif self.norm_type == 'Reg':
+            pass
+        else:
+            raise NotImplementedError
+        return pert
+    
+    def init_pert(self, batch_pert):
+        if self.init_type=='zero':
+            batch_pert.data = batch_pert.data*0
+        elif self.init_type=='random':
+            batch_pert.data = torch.rand_like(batch_pert.data)
+        elif self.init_type=='max':
+            batch_pert.data = batch_pert.data + self.norm_bound
+        elif self.init_type=='min':
+            batch_pert.data = batch_pert.data - self.norm_bound
+        else:
+            raise NotImplementedError
+
+        return self.projection(batch_pert)
+
+    def attack(self, images, labels, max_eps = 1, min_eps = 0):
+        # Set max_eps and min_eps to valid range
+
+        model = self.model
+        model_ref = self.model_ref
+
+        batch_pert = torch.zeros_like(images, requires_grad=True)
+        batch_pert = self.init_pert(batch_pert)
+        model.eval()
+        model_ref.eval()
+
+        for _ in range(self.num_steps):   
+            pert_image = self.pert_func(images, batch_pert)
+            ori_lab = torch.argmax(model.forward(images),axis = 1).long()
+            ori_lab_ref = torch.argmax(model_ref.forward(images),axis = 1).long()
+
+            per_logits = model.forward(pert_image)
+            per_logits_ref = model_ref.forward(pert_image)
+
+            pert_label = torch.argmax(per_logits, dim=1)
+            pert_label_ref = torch.argmax(per_logits_ref, dim=1)
+                
+            success_attack = pert_label != ori_lab
+            success_attack_ref = pert_label_ref != ori_lab_ref
+            common_attack = torch.logical_and(success_attack, success_attack_ref)
+            shared_attack = torch.logical_and(common_attack, pert_label == pert_label_ref)
+
+            # Adversarial loss
+            # use early stop or loss clamp to avoid very large loss
+            loss_adv = torch.tensor(0.0).to(images.device)
+            if torch.logical_not(success_attack).sum()!=0:
+                loss_adv += F.cross_entropy(per_logits, labels, reduction='none')[torch.logical_not(success_attack)].sum()
+            if torch.logical_not(success_attack_ref).sum()!=0:
+                loss_adv += F.cross_entropy(per_logits_ref, labels, reduction='none')[torch.logical_not(success_attack_ref)].sum()
+            loss_adv = - loss_adv/2/images.shape[0]
+
+            # Shared loss
+            # JS divergence version (https://en.wikipedia.org/wiki/Jensen%E2%80%93Shannon_divergence)
+            p_model = F.softmax(per_logits, dim=1).clamp(min=1e-8)
+            p_ref = F.softmax(per_logits_ref, dim=1).clamp(min=1e-8)
+            mix_p = 0.5*(p_model+p_ref)
+            loss_js = 0.5*(p_model*p_model.log() + p_ref*p_ref.log()) - 0.5*(p_model*mix_p.log() + p_ref*mix_p.log())
+            loss_cross = loss_js[torch.logical_not(shared_attack)].sum(dim=1).sum()/images.shape[0]
+
+            # Update pert              
+            batch_pert.grad = None
+            loss_ae = self.beta_1 * loss_adv + self.beta_2 * loss_cross
+            loss_ae.backward()
+
+            batch_pert.data = batch_pert.data - self.step_size * batch_pert.grad.sign()
+    
+            # Projection
+            batch_pert = self.projection(batch_pert)
+
+            # Optimal: projection to S and clip to [min_eps, max_eps] to ensure the perturbation is valid. It is not necessary for backdoor defense as done in i-BAU.
+            # Mannually set the min_eps and max_eps to match the dataset normalization
+            # batch_pert.data = torch.clamp(batch_pert.data, min_eps, max_eps)
+
+            if torch.logical_not(shared_attack).sum()==0:
+                break
+        if self.verbose:
+            #print(f'Maximization End: \n Adv h: {success_attack.sum().item()}, Adv h_0: {success_attack_ref.sum().item()}, Adv Common: {common_attack.sum().item()}, Adv Share: {shared_attack.sum().item()}.\n Loss adv {loss_adv.item():.4f}, Loss share {loss_cross.item():.4f}, Loss total {loss_ae.item():.4f}.\n L1 norm: {torch.sum(batch_pert[0].abs().sum()):.4f}, L2 norm: {torch.norm(batch_pert[0]):.4f}, Linf norm: {torch.max(batch_pert[0].abs()):.4f}')                    
+            pass
+
+        return batch_pert.detach()
