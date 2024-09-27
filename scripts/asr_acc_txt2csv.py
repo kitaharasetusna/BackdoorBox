3b766740b2dac4db37bd9231cf430e455c1e3741
Your Name <zxiong9@stevens.edi>
2024-09-27 12:38:35 -0500

    update txt2csv

--- scripts/asr_acc_txt2csv.py
+++ scripts/asr_acc_txt2csv.py
@@ -1,28 +1,31 @@
 import re
 import csv
 
-exp_dir = '../experiments/exp6_FI_B/SIG' 
-# csv_file = '/ABL_BATT'
-csv_file = '/SAU_SIG'
-txt_file = exp_dir+csv_file+'.txt'
+attacks_ =['ISSBA', 'WaNet','BATT', 'Blended']
+for attack in attacks_:
+    exp_dir = f'../experiments/exp7_TinyImageNet/{attack}/' 
+    # csv_file = '/ABL_BATT'
+    files_ = ['1_B2B', '2_ABL', '3_SAU', '4_FT']
+    for csv_file in files_:
+        txt_file = exp_dir+csv_file+'.txt'
 
 
-import re
+        import re
 
-# Input and output file paths
-input_file = exp_dir+csv_file+'.txt'  # replace with your input file path
-output_file = exp_dir+csv_file+'_out.txt'  # replace with your desired output file path
+        # Input and output file paths
+        input_file = exp_dir+csv_file+'.txt'  # replace with your input file path
+        output_file = exp_dir+csv_file+'_out.txt'  # replace with your desired output file path
 
-# Regular expression pattern to match the line and capture ASR and ACC values
-pattern = r'model - ASR:\s*([\d.]+), ACC:\s*([\d.]+)'
+        # Regular expression pattern to match the line and capture ASR and ACC values
+        pattern = r'model - ASR:\s*([\d.]+), ACC:\s*([\d.]+)'
 
-with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
-    for line in infile:
-        match = re.search(pattern, line)
-        if match:
-            asr_value = match.group(1)
-            acc_value = match.group(2)
-            # Write the values to the output file
-            outfile.write(f"{asr_value} {acc_value}\n")
+        with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+            for line in infile:
+                match = re.search(pattern, line)
+                if match:
+                    asr_value = match.group(1)
+                    acc_value = match.group(2)
+                    # Write the values to the output file
+                    outfile.write(f"{asr_value} {acc_value}\n")
 
-print("Extraction complete! Check output.txt for results.")
+    print("Extraction complete! Check output.txt for results.")
